27ac0ceecad327397da036178f4a4d121185010b
unknown <you@example.com>
2022-12-25 14:50:51 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
 import {useState} from "react";
@@ -12,20 +12,20 @@ export const SearchContext = createContext("");
 function App() {
     const [searchValue, setSearchValue] = useState();
     return (
-        <div className="wrapper">
-            <SearchContext.Provider value={{searchValue,setSearchValue}}>
-                <Header/>
-                <div className="content">
-                    {/*<Router>*/}
+        <HashRouter>
+            <div className="wrapper">
+                <SearchContext.Provider value={{searchValue, setSearchValue}}>
+                    <Header/>
+                    <div className="content">
                         <Routes>
                             <Route path="/" element={<Home searchValue={searchValue}/>}/>
                             <Route path="/card" element={<Cart/>}/>
                             <Route path="*" element={<NotFound/>}/>
                         </Routes>
-                    {/*</Router>*/}
-                </div>
-            </SearchContext.Provider>
-        </div>
+                    </div>
+                </SearchContext.Provider>
+            </div>
+        </HashRouter>
     );
 }
 
